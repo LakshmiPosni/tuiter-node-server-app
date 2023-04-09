73219a4f5ef2fca62f8f87f38d5699b54dffb231
LakshmiPosni <posni.l@northeastern.edu>
2023-04-09 14:03:55 -0400

    a9 work

--- app.js
+++ app.js
@@ -1,17 +1,53 @@
 import express from 'express';
-import HelloController
-    from "./controllers/hello-controller.js"
-import UserController
-    from "./controllers/users/users-controller.js"
-import TuitsController
-    from "./controllers/tuits/tuits-controller.js";
+import mongoose from "mongoose";
 import cors from 'cors'
+import dotenv from "dotenv";
+import HelloController from "./controllers/hello-controller.js"
+import UserController from "./controllers/users/users-controller.js"
+import TuitsController from "./controllers/tuits/tuits-controller.js";
 
-
+// dotenv.config();
 const app = express()
+// mongoose.connect('mongodb://127.0.0.1:27017/tuiter');
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+    || 'mongodb://127.0.0.1:27017/tuiter';
+mongoose.connect(CONNECTION_STRING);
 app.use(cors())
 app.use(express.json());
 TuitsController(app);
 HelloController(app)
 UserController(app)
-app.listen(process.env.PORT || 4000);
+app.listen(process.env.PORT || 4000)
+
+
+
+
+
+
+
+
+
+
+
+// import mongoose from "mongoose";
+// // mongoose.connect('mongodb://127.0.0.1:27017/tuiter');
+// import express from 'express';
+// import HelloController
+//     from "./controllers/hello-controller.js"
+// import UserController
+//     from "./controllers/users/users-controller.js"
+// import TuitsController
+//     from "./controllers/tuits/tuits-controller.js";
+// import cors from 'cors'
+// import dotenv from "dotenv";
+//
+// const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+//     || 'mongodb://127.0.0.1:27017/tuiter'
+// mongoose.connect(CONNECTION_STRING);
+// const app = express()
+// app.use(cors())
+// app.use(express.json());
+// TuitsController(app);
+// HelloController(app)
+// UserController(app)
+// app.listen(process.env.PORT || 4000);
